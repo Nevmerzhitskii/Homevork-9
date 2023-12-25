f8681ebf47a51840e9f103cfce7dfb7885f35881
Nevmerzhitskii <comdeo13@yandex.ru>
2023-12-25 10:36:48 +0900

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+
+
     }
+    public static void task1(){
+        System.out.println("Задача №1");
+        String firstName = "ivan";
+        String middleName = "ivanovich";
+        String lastName = "ivanov";
+        String fullName = lastName + " " + firstName + " " + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника " + fullName );
+
+    }
+    public static void task2(){
+        System.out.println("Задача №2");
+        String firstName = "ivan";
+        String middleName = "ivanovich";
+        String lastName = "ivanov";
+        String fullName = lastName + " " + firstName + " " + " " + middleName;
+        String small = fullName;
+        System.out.println(small.toUpperCase());
+
+    }public static void task3(){
+        System.out.println("Задача №3");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + " " + middleName;
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
+
+
+    }
+
 }
